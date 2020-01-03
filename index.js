cce15c5d479765b32b52f7683e64da8f0d3a2f29
Faizan AH <faizan@aiondigital.com>
2020-01-03 23:22:49 +0500

    added arabic errrs

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const errors = require('./errors.json');
+const errors = require("./errors.json");
 
 class AppError extends Error {
-    constructor(type = 'SERVER_ERROR', params) {
-        super(params);
-        if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, AppError);
-        }
-        if (errors[type]) {
-            this.name = errors[type].name;
-            this.description = params || errors[type].description;
-            this.code = errors[type].code;
-            this.status = errors[type].status;
-            this.date = new Date();
-        } else {
-            this.code = type;
-            this.status = 200;
-            this.description = params;
-        }
+  constructor(type = "SERVER_ERROR", params) {
+    super(params);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, AppError);
     }
+    if (errors[type]) {
+      this.name = errors[type].name;
+      this.description =
+        params.message || errors[type].description[params.lang || "en"];
+      this.code = errors[type].code;
+      this.status = errors[type].status;
+      this.date = new Date();
+    } else {
+      this.code = type;
+      this.status = 200;
+      this.description = params;
+    }
+  }
 }
 module.exports = AppError;
