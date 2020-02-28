056ee5dedb86ece630d33663a86490d0847f383e
Faizan AH <faizan@aiondigital.com>
2020-02-28 22:55:21 +0500

    update package errors

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ class AppError extends Error {
     if (errors[type]) {
       this.name = errors[type].name;
       this.description =
-        params ? params.message : '--';
+        (params && params.message) ? params.message : errors[type].description;
       this.code = errors[type].code;
       this.status = errors[type].status;
-      this.date = new Date();
     } else {
       this.name = type;
       this.code = type;
       this.status = 200;
-      this.date = new Date();
-      this.description = params;
+      this.description = (params && params.message) ? {en: params['message'], ar: ''} : {};
     }
+    this.date = new Date();
   }
+  
 }
 module.exports = AppError;
